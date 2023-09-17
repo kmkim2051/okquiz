5038030a777ea69fee53f04cf0cc249fcad2b9ff
kimkyungmin <qaws2051@gmail.com>
2023-09-17 19:20:29 +0900

    초기 설정

---
+++ src/main/java/com/okintern3/OkIntern3Application.java
@@ -0,0 +1,13 @@
+package com.okintern3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkIntern3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkIntern3Application.class, args);
+	}
+
+}
