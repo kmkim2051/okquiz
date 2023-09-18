bf4cb52c468fdfa97fa4e74566dfa806aedecd69
kimkyungmin <qaws2051@gmail.com>
2023-09-18 13:35:45 +0900

    Test: 퀴즈 및 퀴즈 옵션 생성 테스트

--- src/test/java/com/okintern3/OkIntern3ApplicationTests.java
+++ src/test/java/com/okintern3/OkIntern3ApplicationTests.java
@@ -1,13 +1,50 @@
 package com.okintern3;
 
+import com.okintern3.entity.Category;
+import com.okintern3.entity.Quiz;
+import com.okintern3.entity.QuizOption;
+import com.okintern3.entity.QuizType;
+import com.okintern3.repository.CategoryRepository;
+import com.okintern3.repository.QuizRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class OkIntern3ApplicationTests {
 
+	@Autowired
+	private QuizRepository quizRepository;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void 퀴즈_생성_확인() {
+		// given
+		Category category = new Category("클라우드");
+
+		Quiz quiz = new Quiz("question1", category, QuizType.MULTI_OPTION);
+		QuizOption quizOption = new QuizOption("answer", true, quiz);
+		QuizOption quizOption2 = new QuizOption("notanswer1", false, quiz);
+		QuizOption quizOption3 = new QuizOption("notanswer2", false, quiz);
+		QuizOption quizOption4 = new QuizOption("notanswer3", false, quiz);
+
+		quizRepository.save(quiz);
+
+		// when
+		Quiz findQuiz = quizRepository.findByCategory(category).get(0);
+
+		// then
+		Assertions.assertThat(findQuiz.getQuestion()).isEqualTo("question1");
+		Assertions.assertThat(findQuiz.getQuizType()).isEqualTo(QuizType.MULTI_OPTION);
 	}
 
 }
