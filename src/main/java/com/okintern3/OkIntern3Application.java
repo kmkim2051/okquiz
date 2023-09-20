a476019d8227f45a11f39058f342fced2afde02b
kimkyungmin <qaws2051@gmail.com>
2023-09-20 21:10:08 +0900

    fix: JpaAuditing 누락된것 수정

--- src/main/java/com/okintern3/OkIntern3Application.java
+++ src/main/java/com/okintern3/OkIntern3Application.java
@@ -1,9 +1,12 @@
 package com.okintern3;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OkIntern3Application {
 
 	public static void main(String[] args) {
