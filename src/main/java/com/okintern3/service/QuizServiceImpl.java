047701132b23b5735dbd8e9f97311c1961616b02
kimkyungmin <qaws2051@gmail.com>
2023-09-21 22:00:06 +0900

    feat: 퀴즈 생성 비즈니스 로직 유효성 검증 및 예외처리 추가

--- src/main/java/com/okintern3/service/QuizServiceImpl.java
+++ src/main/java/com/okintern3/service/QuizServiceImpl.java
@@ -19,6 +19,9 @@ import com.okintern3.exception.QuizNotFoundException;
 import com.okintern3.repository.CategoryRepository;
 import com.okintern3.repository.QuizLogRepository;
 import com.okintern3.repository.QuizRepository;
+import com.okintern3.dto.QuizOptionRequest;
+import com.okintern3.entity.QuizType;
+import com.okintern3.exception.InvalidQuizOptionException;
 
 @Service
 @RequiredArgsConstructor
@@ -30,11 +33,44 @@ public class QuizServiceImpl implements QuizService {
 
     private final QuizLogRepository quizLogRepository;
 
+    private void validateNumberOfOptionsForQuizType(QuizCreateRequest request) {
+        QuizType quizType = request.getQuizType();
+        int numberOfOptions = request.getOptions().size();
+
+        if ((quizType.equals(QuizType.OX)) && (numberOfOptions != 2)) {
+            throw new InvalidQuizOptionException("OX퀴즈의 선택지는 2개여야 합니다.");
+        }
+
+        if ((quizType.equals(QuizType.MULTI_OPTION)) && (numberOfOptions < 2)) {
+            throw new InvalidQuizOptionException("다지선다 퀴즈의 선택지는 2개 이상이어야 합니다.");
+        }
+
+        // 단답형 퀴즈는 복수 정답 인정을 위해 답이 여러 개일 수 있어 유효성 검사 로직이 애매하여 추가하지 않았음
+    }
+
+    private void validateQuizContent(QuizCreateRequest request) {
+        QuizType quizType = request.getQuizType();
+        List<QuizOptionRequest> options = request.getOptions();
+
+        if (quizType.equals(QuizType.OX)) {
+            List<String> contents = options.stream()
+                    .map(QuizOptionRequest::getContent)
+                    .map(String::toUpperCase)
+                    .toList();
+            if (!(contents.contains("O") && contents.contains("X"))) {
+                throw new InvalidQuizOptionException("OX 퀴즈의 선택지는 O와 X를 모두 포함해야 합니다.");
+            }
+        }
+    }
+
     @Override
     @Transactional
     public void createQuiz(QuizCreateRequest request) {
         Category category = categoryRepository.findByName(request.getCategoryName())
                 .orElseThrow(() -> new CategoryNotFoundException("존재하지 않는 카테고리 이름입니다."));
+
+        validateNumberOfOptionsForQuizType(request);
+        validateQuizContent(request);
 
         Quiz quiz = new Quiz(
                 request.getQuestion(),
@@ -44,7 +80,7 @@ public class QuizServiceImpl implements QuizService {
         request.getOptions()
                 .forEach(option -> new QuizOption(
                         option.getContent(),
-                        option.isAnswer(),
+                        option.getIsAnswer(),
                         quiz,
                         option.getDescription()
                 ));
